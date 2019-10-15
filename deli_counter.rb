fc3772526224b68a6a3a8942a01539e7cda3b710
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-15 03:16:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,6 +17,9 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-
-puts "Currently serving ."
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else katz_deli.
+    puts "Currently serving ."
+  end
 end
