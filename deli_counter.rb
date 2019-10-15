e2ab6af012bbb924c36bf20dfd1d9b8d46728c88
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-15 02:38:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,7 @@
 katz_deli = []
 
 def line(people)
-  line_array = []
- if people == 0 
-   puts "The line is currently empty."
- else people.each_with_index do |name, index|
-   line_array.push(#{index + 1}, #{name})
-    end
-    puts "The line is currently: #{line_array.join(" ")}"
+  
 end 
 
 def take_a_number(katz_deli, name) 
