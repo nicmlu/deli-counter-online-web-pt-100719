0447dad23a9e41f4a58f5795d43800a9e4155819
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-15 02:51:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,3 +19,17 @@
 # end
 # puts "The line is currently: #{line_array.join(" ")}"
 # end
+
+katz_deli = []
+
+def line(x)
+  line_array = []
+  if x.length == 0
+    puts "The line is currently empty."
+  else
+    x.each.with_index(1) do |name, index|
+      line_array.push("#{index}. #{name}")
+    end
+    puts "The line is currently: #{line_array.join(" ")}"
+  end
+end
