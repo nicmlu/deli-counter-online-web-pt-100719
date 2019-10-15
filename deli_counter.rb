c6052ac69def8cd5cdb3bbe7581d2641dbb649cb
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-15 02:58:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,8 +13,8 @@ def line(people)
 line_array = []
   if people.length == 0 
     puts "The line is currently empty."
-  else people.each.with_index do |name, index|
-    line_array.push("#{index}. #{name}")
+  else people.each_with_index do |name, index|
+    line_array.push("#{index + 1}. #{name}")
   end
   puts "The line is currently: #{line_array.join(" ")}"
 end
