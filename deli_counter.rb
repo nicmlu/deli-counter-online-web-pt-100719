231940bb9a465bdf1a46d044305696d373afd1eb
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-15 02:59:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,35 +1,12 @@
 katz_deli = []
 
-# # def line(people)
-# #   line_array = []
-# #   if people == 0 
-# #     puts "The line is currently empty."
-# #   else people.each_with_index {|name, index| line_array.push(#{index + 1}, #{name})}
-# #   end
-# #   puts "The line is currently: #{line_array.join('')}"
-# # end
-
 def line(people)
 line_array = []
   if people.length == 0 
     puts "The line is currently empty."
   else people.each_with_index do |name, index|
     line_array.push("#{index + 1}. #{name}")
-  end
+    end
   puts "The line is currently: #{line_array.join(" ")}"
+  end
 end
-end
-
-katz_deli = []
-
-# def line(x)
-#   line_array = []
-#   if x.length == 0
-#     puts "The line is currently empty."
-#   else
-#     x.each.with_index(1) do |name, index|
-#       line_array.push("#{index}. #{name}")
-#     end
-#     puts "The line is currently: #{line_array.join(" ")}"
-#   end
-# end
