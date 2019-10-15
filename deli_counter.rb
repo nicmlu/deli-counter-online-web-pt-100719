3e934b767b922f105e90fbbe18b45acdd3432c87
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-15 03:14:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving
+def now_serving(katz_deli)
 
 puts "Currently serving #{name}."
 end
