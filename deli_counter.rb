2eede2b38c59f6bbcdfcce070a9183360b96ab40
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-15 02:47:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,3 +8,9 @@ katz_deli = []
 #   end
 #   puts "The line is currently: #{line_array.join('')}"
 # end
+
+def line(people)
+line_array = []
+  if line_array.length == 0 
+    puts "The line is currently empty."
+end
