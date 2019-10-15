2a93706109ebba22bb4eed3a7c504b06bd5fe5fd
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-15 02:52:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,4 @@
-# katz_deli = []
+katz_deli = []
 
 # # def line(people)
 # #   line_array = []
@@ -9,27 +9,27 @@
 # #   puts "The line is currently: #{line_array.join('')}"
 # # end
 
-# def line(people)
-# line_array = []
-#   if people.length == 0 
-#     puts "The line is currently empty."
-#   else people.each_with_index do |name, index|
-#     line_array.push(#{index + 1}, #{name})
-#   end
-# end
-# puts "The line is currently: #{line_array.join(" ")}"
-# end
+def line(people)
+line_array = []
+  if people.length == 0 
+    puts "The line is currently empty."
+  else people.each_with_index do |name, index|
+    line_array.push("#{index + 1}, #{name}")
+  end
+end
+puts "The line is currently: #{line_array.join(" ")}"
+end
 
 katz_deli = []
 
-def line(x)
-  line_array = []
-  if x.length == 0
-    puts "The line is currently empty."
-  else
-    x.each.with_index(1) do |name, index|
-      line_array.push("#{index}. #{name}")
-    end
-    puts "The line is currently: #{line_array.join(" ")}"
-  end
-end
+# def line(x)
+#   line_array = []
+#   if x.length == 0
+#     puts "The line is currently empty."
+#   else
+#     x.each.with_index(1) do |name, index|
+#       line_array.push("#{index}. #{name}")
+#     end
+#     puts "The line is currently: #{line_array.join(" ")}"
+#   end
+# end
